4d8a37b9d5df103c936df9b0f6713c4cd95f6847
Yiesha Pan <1085005648@qq.com>
2018-06-12 16:08:37 +0800

    first commit

---
+++ src/components/Customes.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="customes container">
+    <Alert :message="alert" v-if="alert"></Alert>
+    <h1 class="page-header">用户管理系统</h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>姓名</th>
+                <th>电话</th>
+                <th>邮箱</th>
+                <th></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for='customer in customers'>
+                <td>{{customer.name}}</td>
+                <td>{{customer.phone}}</td>
+                <td>{{customer.email}}</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+import Alert from './Alert'
+
+export default {
+  name: 'customes',
+  data () {
+    return {
+      customers:[],
+      alert:""
+    }
+  },
+  methods:{
+    fetchCustomers(){
+        this.$http.get('http://localhost:3000/users')
+            .then(function(response){
+                this.customers=response.body;
+            })
+    }
+  },
+  created(){
+    this.fetchCustomers();
+  },
+  updated(){
+    this.fetchCustomers();
+  },
+  components:{
+    Alert
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
